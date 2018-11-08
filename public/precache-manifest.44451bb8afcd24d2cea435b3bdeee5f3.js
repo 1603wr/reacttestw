c81f90609298428c9a8eaea5d2dba2b6d294309e
wangrui <1209226177@qq.com>
2018-11-08 14:33:51 +0800

    '2222'

---
+++ public/precache-manifest.44451bb8afcd24d2cea435b3bdeee5f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f8c94d5d8da70a0edb2",
+    "url": "/static/js/main.5f8c94d5.chunk.js"
+  },
+  {
+    "revision": "ccd03424ef274fc9636f",
+    "url": "/static/js/1.ccd03424.chunk.js"
+  },
+  {
+    "revision": "5f8c94d5d8da70a0edb2",
+    "url": "/static/css/main.6b607036.chunk.css"
+  },
+  {
+    "revision": "1dde2d05f04ad20431a52aecccb98ba8",
+    "url": "/index.html"
+  }
+];
